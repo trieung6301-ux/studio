cd74446362c6caca057809a7d4a7ed19285a7f45
trieung6301 <trieung6301@gmail.com>
2025-10-08 09:02:09 +0700

    fix(tailwind): update headline font from "Archivo Black" to "Roboto" for improved typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['"Archivo Black"', 'sans-serif'],
+        headline: ['Roboto', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
